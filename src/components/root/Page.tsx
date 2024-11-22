a80c7da66c3efa3b015a9c69773db2cc60d9985c
minhyeong <minhyeong.dev@gmail.com>
2024-11-22 20:51:47 +0900

    [Feat] set root page
    Feat: 음성을 저장받는 기능까지 구현 완료

--- src/components/root/Page.tsx
+++ src/components/root/Page.tsx
@@ -1,3 +1,27 @@
+import { useState } from "react";
+import { cn } from "fast-jsx/util";
+import ButtonProvider from "./organism/ButtonProvider";
+
 export default function RootPage() {
-  return <div>asdf</div>;
+  const [audioUrl, setAudioUrl] = useState<string>("");
+
+  const container = {
+    displays: "flex items-center justify-center",
+    sizes: "w-full min-h-screen",
+  };
+
+  return (
+    <div className={cn(container)}>
+      <ButtonProvider audioUrlState={[audioUrl, setAudioUrl]} />
+      {audioUrl && (
+        <div>
+          <h3>Recorded Audio:</h3>
+          <audio controls src={audioUrl}></audio>
+          <a href={audioUrl} download="recording.webm">
+            Download Audio
+          </a>
+        </div>
+      )}
+    </div>
+  );
 }
